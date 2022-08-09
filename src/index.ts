8a7657cea09359d482a630eb6613b338925341e3
Mick F <mick@mickf.net>
2022-08-09 13:04:35 +0200

    feat: 🎸 Add option for "User-Agent" header

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,32 @@ import { IncomingHttpHeaders } from "http";
 import { get } from "https";
 
 type FetchResponse = {
-  headers: IncomingHttpHeaders,
-  statusCode: number,
-  body: string
-}
+  headers: IncomingHttpHeaders;
+  statusCode: number;
+  body: string;
+};
 
 /**
- * Fetches the given URL as a simple `GET` request. 
- * 
- * Node should bring native support of the Fetch API so this method is scheduled to become 
+ * Fetches the given URL as a simple `GET` request.
+ *
+ * Node should bring native support of the Fetch API so this method is scheduled to become
  * obsolete pretty soon.
- * 
+ *
  * @param url The URL to fetch.
+ * @param userAgent The content of the `User-Agent` header sent with the request.
  * @returns The response to the request (ie headers, status code and body).
  */
-const fetch = (url: string | URL): Promise<FetchResponse> => {
+const fetch = (
+  url: string | URL,
+  userAgent: string
+): Promise<FetchResponse> => {
   return new Promise((resolve, reject) => {
-      get(url, (res) => {
+    get(
+      url,
+      {
+        headers: { "User-Agent": userAgent },
+      },
+      (res) => {
         const { headers, statusCode } = res;
 
         assert(!!statusCode);
@@ -32,10 +41,10 @@ const fetch = (url: string | URL): Promise<FetchResponse> => {
         res.on("end", () => {
           resolve({ statusCode, headers, body });
         });
-      })
-      .on("error", (e) => {
-        reject(e);
-      });
+      }
+    ).on("error", (e) => {
+      reject(e);
+    });
   });
 };
 
